a8f46067780f395a26c3995f6e847ecb24a2f3da
TubiOb <Tubiobaloluwa@gmail.com>
2023-12-10 00:35:50 +0100

    sorting out the password input box

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from '../assets/Bullet journal-rafiki.svg'
 import '../index.css'
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 const Login = () => {
+   const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+  };
+
   return (
     <div className='flex flex-row items-center justify-between w-full h-screen'>
         <div className='w-[40%] h-full hidden md:flex items-center justify-end'>
@@ -11,14 +18,38 @@ const Login = () => {
 
         <div className='w-full md:w-[60%] h-full flex items-center justify-center'>
 
-          <div className="rounded-xl bg-blue-50 lg:w-[60%] md:w-[80%] w-[90%] flex flex-col font-['Lato'] items-center">
-            <div className='flex items-center flex-col w-[80%] md:w-[50%] text-center p-1 gap-1'>
+          <div className="rounded-xl bg-blue-100 lg:w-[50%] md:w-[80%] w-[90%] h-auto py-2 px-3 gap-4 flex flex-col font-['Lato'] items-center">
+            <div className='flex items-center flex-col w-[95%] md:w-[80%] text-center p-2 gap-1'>
               <h4 className='text-lg md:text-xl lg:text-2xl font-semibold'>Welcome,</h4>
               <h4 className='text-lg md:text-xl lg:text-2xl font-normal'>Glad to see you!</h4>
               <p className='text-sm md:text-base lg:text-lg'>Hey, Enter your details to get signed in to your account</p>
             </div>
 
-            <form>
+            <form className='w-[95%] md:w-[80%] mt-2 flex flex-col justify-between gap-6'>
+              <label htmlFor="Username" className="relative block rounded-lg border w-full focus-within:border-white outline-none">
+                <input type="text" id="Username" className="peer border-none bg-blue-50 w-full focus:bg-blue-100 placeholder-transparent py-2 md:py-1.5 lg:py-1 px-2 text-xs md:text-sm lg:text-base focus:border-transparent focus:outline-none rounded-lg focus:ring-0" placeholder="Username"/>
+                <span className="pointer-events-none absolute start-3.5 bg-transparent backdrop-blur-sm peer-focus:bg-blue-100 top-0 -translate-y-1/2 p-0.5 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">Username</span>
+              </label>
+
+              <label htmlFor="Email" className="relative block rounded-lg border w-full focus-within:border-white outline-none">
+                <input type="email" id="Email" className="peer border-none bg-blue-50 w-full focus:bg-blue-100 placeholder-transparent py-2 md:py-1.5 lg:py-1 px-2 text-xs md:text-sm lg:text-base focus:border-transparent focus:outline-none rounded-lg focus:ring-0" placeholder="Email"/>
+                <span className="pointer-events-none absolute start-3.5 bg-transparent backdrop-blur-sm peer-focus:bg-blue-100 top-0 -translate-y-1/2 p-0.5 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">Email</span>
+              </label>
+
+              <label htmlFor="Password" className="relative block rounded-lg border w-full focus-within:border-white outline-none">
+                <div className='flex items-center'>
+                  <input type="password" id="Password" className="peer border-none bg-blue-50 w-full focus:bg-blue-100 placeholder-transparent py-2 md:py-1.5 lg:py-1 px-2 text-xs md:text-sm lg:text-base focus:border-transparent focus:outline-none rounded-lg focus:ring-0" placeholder="Password"/>
+                </div>
+                
+                <span className="pointer-events-none absolute start-3.5 bg-transparent backdrop-blur-sm peer-focus:bg-blue-100 top-0 -translate-y-1/2 p-0.5 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">Password</span>
+                <div className='w-5 h-5 relative secure'>
+                  {passwordVisible ? (
+                  <AiOutlineEyeInvisible className="cursor-pointer left-0 w-5 h-5 mr-1"  onClick={togglePasswordVisibility} />
+                  ) : (
+                  <AiOutlineEye className="cursor-pointer left-0 w-5 h-5 mr-1"  onClick={togglePasswordVisibility} />
+                  )}
+                </div>
+              </label>
               
             </form>
           </div>
