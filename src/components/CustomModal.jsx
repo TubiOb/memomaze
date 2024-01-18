8338bee743e19e5a7ebe65b0c048566a845d78c1
TubiOb <Tubiobaloluwa@gmail.com>
2024-01-18 11:13:18 +0100

    updated the custom modal logic

--- src/components/CustomModal.jsx
+++ src/components/CustomModal.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
 import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, FormControl, FormLabel, Input, Button, } from '@chakra-ui/react';
 
-const CustomModal = ({isOpen, onClose, initialRef, finalRef}) => {
+const CustomModal = ({isOpen, onClose}) => {
   return (
     <div>
-        <Modal isOpen={isOpen} onClose={onClose} isCentered size={['xs', 'md', 'lg']} colorScheme='teal'>
-            <ModalOverlay backdropFilter='blur(8px) hue-rotate(90deg)' />
-                <ModalContent>
-                <ModalHeader>Create your account</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody pb={6}>
-                    <FormControl>
-                        <FormLabel>First name</FormLabel>
-                        <Input ref={initialRef} placeholder='First name' />
-                    </FormControl>
+        <Modal isOpen={isOpen} onClose={onClose} isCentered size={['xs', 'md', 'lg']} colorScheme='teal' >
+            <ModalOverlay backdropFilter='blur(5px) hue-rotate(30deg)' />
+                <ModalContent colorScheme='teal'>
+                    <ModalHeader>Create your account</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        <FormControl>
+                            <FormLabel>First name</FormLabel>
+                            <Input ref={initialRef} placeholder='First name' />
+                        </FormControl>
 
-                    <FormControl mt={4}>
-                        <FormLabel>Last name</FormLabel>
-                        <Input placeholder='Last name' />
-                    </FormControl>
-                </ModalBody>
+                        <FormControl mt={4}>
+                            <FormLabel>Last name</FormLabel>
+                            <Input placeholder='Last name' />
+                        </FormControl>
+                    </ModalBody>
 
-                <ModalFooter>
-                    <Button colorScheme='blue' mr={3}>
-                    Save
-                    </Button>
-                    <Button onClick={onClose}>Cancel</Button>
-                </ModalFooter>
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3}>
+                        Save
+                        </Button>
+                        <Button onClick={onClose}>Cancel</Button>
+                    </ModalFooter>
                 </ModalContent>
         </Modal>
     </div>
