59b80fbad8a7a1cdf74e9401881df3e556ba24d5
TubiOb <Tubiobaloluwa@gmail.com>
2024-02-27 22:17:01 +0100

    testing the responsiveness of the masonry layout

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -435,7 +435,7 @@ const HomeLayout = ({ updateFolderOptions, updateFileOptions }) => {
             
 
             <Box w='full' maxW='100%' display='flex' alignContent='center' justifyContent='center' flexGrow='grow' className='h-screen' >
-                <Box className="columns-2 md:columns-3 lg:columns-4 xl:columns-5 2xl:columns-6 mx-auto gap-3 items-start py-2 px-3 w-full pb-10 space-y-3 min-h-screen overflow-y-auto" overflowY='auto'>
+                <Box className="columns-2 md:columns-3 lg:columns-4 xl:columns-5 2xl:columns-6 mx-auto gap-3 items-start py-2 px-3 w-full pb-10 space-y-3 flex-grow min-h-full overflow-y-auto" overflowY='auto'>
                     {files.filter((file) => file.name.toLowerCase().includes(searchQuery.toLowerCase())).map((file) => (
                         <React.Fragment key={file.id}>
                             <div className='group items-start max-w-[145px] md:max-w-[200px] max-h-[350px] break-inside-avoid rounded-md shadow-md shadow-neutral-600/40 dark:shadow-white/10 hover:shadow-neutral-600/80 dark:hover:shadow-white/40 border border-neutral-50/25 overflow-hidden'>
